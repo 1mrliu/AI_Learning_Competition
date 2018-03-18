acdcb6f15cb6d22129ac2ab2b2777b68bc4acb83
1mrliu <498744838@qq.com>
2018-03-18 21:50:53 +0800

    提升titanic的预测，调整的是svc的gama的值，特征的处理没有突破。kaggle评分0.80382

--- DonorsChoose.org_Competition/DonorsChoose_Main.py
+++ DonorsChoose.org_Competition/DonorsChoose_Main.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
-
+# 加载训练集
 train_data = pd.read_csv('/Users/liudong/Desktop/DonorsChoose/train.csv')
-
 print(train_data)
 
+# 加载测试集
 test_data  = pd.read_csv('/Users/liudong/Desktop/DonorsChoose/test.csv')
 print(test_data)
 
+# 加载资源集
 resource_data = pd.read_csv('/Users/liudong/Desktop/DonorsChoose/resources.csv')
 print(resource_data)
--- Titanic_Competition/titanic.py
+++ Titanic_Competition/titanic.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """
+Created: LiuDong
 """
 
 import numpy as np
@@ -14,12 +15,13 @@ from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_score
 
 
+
 # 0. 数据读入及预处理
 data_train = pd.read_csv('/Users/liudong/Desktop/titanic/train.csv')
 
 
 # 1. 去除唯一属性特 axis=1 表示去除PassengerId 和 Ticket列  inplace为真的时候，不返回数据，直接内部执行
-data_train.drop(['PassengerId', 'Ticket'], axis=1, inplace=True)
+data_train.drop(['PassengerId'], axis=1, inplace=True)
 
 # 2. 类别特征One-Hot编码
 data_train['Sex'] = data_train['Sex'].map({'female': 0, 'male': 1}).astype(np.int64)
@@ -44,9 +46,10 @@ data_train = pd.concat([data_train, pd.get_dummies(data_train.Name)], axis=1)
 data_train = data_train.rename(columns={'Miss': 'Name_Miss','Mr': 'Name_Mr',
                                         'Mrs': 'Name_Mrs','Other': 'Name_Other'})
 
-# print(data_train)
 
 # 3. 数值特征标准化
+# 因为数据中的年龄的值差距比较大，会影响到梯度下降和收敛
+# 因此采用StandardScaler()将数据进行标准化处理
 def fun_scale(df_feature):
     np_feature = df_feature.values.reshape(-1,1).astype(np.float64)
     feature_scale = StandardScaler().fit(np_feature)
@@ -55,9 +58,9 @@ def fun_scale(df_feature):
 
 
 Pclass_scale, data_train['Pclass_scaled'] = fun_scale(data_train['Pclass'])
-Fare_scale, data_train['Fare_scaled'] = fun_scale(data_train['Fare'])
-SibSp_scale, data_train['SibSp_scaled'] = fun_scale(data_train['SibSp'])
-Parch_scale, data_train['Parch_scaled'] = fun_scale(data_train['Parch'])
+Fare_scale, data_train['Fare_scaled']     = fun_scale(data_train['Fare'])
+SibSp_scale, data_train['SibSp_scaled']   = fun_scale(data_train['SibSp'])
+Parch_scale, data_train['Parch_scaled']   = fun_scale(data_train['Parch'])
 
 # 4. 缺失值补全及相应处理(使用随机森林方法进行补全未知的年龄值)
 # 处理Age缺失值并标准化
@@ -68,7 +71,7 @@ def set_missing_feature(train_for_missingkey, data, info):
     y = known_feature[:, 0] # 第1列作为待补全属性
     x = known_feature[:, 1:] # 第2列及之后的属性作为预测属性
     # 随机森林回归  第一个参数是随机数  第二个参数是在森林中树的个数
-    rf = RandomForestRegressor(random_state=0, n_estimators=100)
+    rf = RandomForestRegressor(random_state=0, n_estimators=2000, n_jobs=-1)
     # 使用贝叶斯获得的准确度较低
     # rf = BayesianRidge(n_iter=500)
     # 从训练集建立森林
@@ -78,22 +81,18 @@ def set_missing_feature(train_for_missingkey, data, info):
     data.loc[data.Age.isnull(), 'Age'] = predictage
     return data
 
-
 # 处理Cabin(船舱)特征
 # 船舱编号为空设置为0，不为空设置为1
 def set_Cabin_type(df):
-    df.loc[ (df.Cabin.notnull()), 'Cabin' ] = 1
-    df.loc[ (df.Cabin.isnull()), 'Cabin' ] = 0
+    df.loc[(df.Cabin.notnull()), 'Cabin' ] = 1
+    df.loc[(df.Cabin.isnull()), 'Cabin' ] = 0
     return df
 
-
-train_for_missingkey_train = data_train[['Age','Survived','Sex','Name_Miss','Name_Mr','Name_Mrs',
-                                         'Name_Other','Fare_scaled','SibSp_scaled','Parch_scaled']]
+train_for_missingkey_train = data_train[['Age','Survived','Sex','Name_Miss','Name_Mr','Name_Mrs','Name_Other','Fare_scaled','SibSp_scaled','Parch_scaled']]
 data_train = set_missing_feature(train_for_missingkey_train, data_train,'Train_Age')
 Age_scale, data_train['Age_scaled'] = fun_scale(data_train['Age'])
-
-
 data_train = set_Cabin_type(data_train)
+
 
 
 # 5. 整合数据
@@ -103,8 +102,8 @@ train_y = data_train['Survived'].as_matrix()
 
 
 # 6. 模型搭建及交叉验证
-lr = LogisticRegression(C=1.0, tol=1e-6)
-svc = SVC(C=1.8, kernel='rbf', decision_function_shape='ovo')
+lr = LogisticRegression(C=1.0, penalty='l1', tol=1e-6)
+svc = SVC(C=1.8, kernel='rbf', gamma=0.1, decision_function_shape='ovo')
 adaboost = AdaBoostClassifier(n_estimators=490, random_state=0)
 randomf = RandomForestClassifier(n_estimators=185, max_depth=5, random_state=0)
 gbdt = GradientBoostingClassifier(n_estimators=436, max_depth=5, random_state=0)
@@ -132,11 +131,11 @@ data_test['Sex'] = data_test['Sex'].map({'female': 0, 'male': 1}).astype(np.int6
 data_test = pd.concat([data_test, pd.get_dummies(data_test.Embarked)], axis=1)
 data_test = data_test.rename(columns={'C': 'Cherbourg','Q': 'Queenstown','S': 'Southampton'})
 
+# 对姓名的称呼进行匹配
 data_test['Name'] = data_test['Name'].map(lambda x:replace_name(x))
 data_test = pd.concat([data_test, pd.get_dummies(data_test.Name)], axis=1)
 data_test = data_test.rename(columns={'Miss': 'Name_Miss','Mr': 'Name_Mr',
                                       'Mrs': 'Name_Mrs','Other': 'Name_Other'})
-print(data_train)
 
 # 测试集标准化函数
 def fun_test_scale(feature_scale, df_feature):
@@ -151,44 +150,41 @@ data_test['SibSp_scaled'] = fun_test_scale(SibSp_scale, data_test['SibSp'])
 data_test['Parch_scaled'] = fun_test_scale(Parch_scale, data_test['Parch'])
 
 # 处理测试集Age缺失值并归一化
-
-
-
 train_for_missingkey_test = data_test[['Age','Sex','Name_Miss','Name_Mr','Name_Mrs','Name_Other',
                                        'Fare_scaled','SibSp_scaled','Parch_scaled']]
 data_test = set_missing_feature(train_for_missingkey_test, data_test, 'Test_Age')
 data_test['Age_scaled'] = fun_test_scale(Age_scale, data_test['Age'])
-
 data_test = set_Cabin_type(data_test)
-
 
 test_X = data_test[['Sex','Cabin','Cherbourg','Queenstown','Southampton','Name_Miss','Name_Mr','Name_Mrs','Name_Other',
                     'Pclass_scaled','Fare_scaled','SibSp_scaled','Parch_scaled','Age_scaled']].as_matrix()
 
+
 # 8. 模型预测
+# 模型融合---目的就是多个模型对数据集进行处理，看看哪个的结果比较好
 model = classifierlist[4]
 # 选择分类器
-print("Test in %s!" % model[0])
-predictions = model[1].predict(test_X).astype(np.int32)
+print("Test in lr!")
+predictions = lr.predict(test_X).astype(np.int64)
 result = pd.DataFrame({'PassengerId':data_test['PassengerId'].as_matrix(), 'Survived':predictions})
-result.to_csv('Result_with_%s.csv' % model[0], index=False)
+result.to_csv('/Users/liudong/Desktop/TITANIC/Result_with_%s.csv' % model[0], index=False)
 
-
-
-print("Test in svc")
-predictions = svc.predict(test_X).astype(np.int32)
+print("Test in GBDT!")
+predictions = gbdt.predict(test_X).astype(np.int64)
 result = pd.DataFrame({'PassengerId':data_test['PassengerId'].as_matrix(), 'Survived':predictions})
-result.to_csv('Result_with_%s.csv' % 'SVC', index=False)
+result.to_csv('/Users/liudong/Desktop/TITANIC/Result_with_%s.csv' % 'SVC', index=False)
 
 
+
+print("Test in SVC!")
+predictions = svc.predict(test_X).astype(np.int64)
+result = pd.DataFrame({'PassengerId':data_test['PassengerId'].as_matrix(), 'Survived':predictions})
+result.to_csv('/Users/liudong/Desktop/TITANIC/Result_with_%s.csv' % 'SVC', index=False)
 
 print("Save xgboost!!!")
-
-xgbpred_test = xgbClassifier.predict(test_X).astype(np.int32)
+xgbpred_test = xgbClassifier.predict(test_X).astype(np.int64)
 result = pd.DataFrame({'PassengerId':data_test['PassengerId'].as_matrix(), 'Survived':xgbpred_test})
-result.to_csv('Result_with_%s.csv' % 'XGBoost', index=False)
-
-
+result.to_csv('/Users/liudong/Desktop/TITANIC/Result_with_%s.csv' % 'XGBoost', index=False)
 print('...\nAll Finish!')
 
 
