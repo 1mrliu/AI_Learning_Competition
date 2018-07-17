ee45e46889ce4745a4eeffd462c743fd825bf192
liudong <ld815626@hotmail.com>
2018-07-17 12:56:05 +0800

    Update LSTM_example.py

--- Tensorflow/LSTM_example.py
+++ Tensorflow/LSTM_example.py
@@ -4,23 +4,34 @@ from collections import namedtuple
 import tensorflow as tf
 import numpy as np 
 
-# 读取数据预处理
+'''
+读取数据 
+使用集合进行去重
+对文档中出现的字母和符号进行编号
+
+'''
 with open('D:/Users/sangfor/Desktop/anna.txt', 'r') as f:
     text = f.read()
 # print(text)
+# 使用set集合是为了去重
 vocab = set(text)
 # print(vocab)
 vocab_to_int = {c:i for i,c in enumerate(vocab)}
 print(vocab_to_int)
 int_to_vocab = dict(enumerate(vocab))
-# print(int_to_vocab)
-# 将txt文件转化为数字编号的文件
+print(int_to_vocab)
+# 将txt文件转化为数字编号的文件 
+# 将文件转化为数字数组
+# for  c in text:
+#    print(c)  c 代表的是文章中使用到的字母和符号
 encoded = np.array([vocab_to_int[c] for c in text], dtype=np.int32)
+print("_____________________________")
+print(encoded) # 输出的是文章中文字代表的字母
 
 # 分割batch
 def get_batches(arr,n_seqs,n_steps):
     '''
-    arr:数组
+    生成数据的arr:数组
     n_seqs:一个batch中的序列个数
     n_steps:单个序列包含的字符
     '''
@@ -28,8 +39,9 @@ def get_batches(arr,n_seqs,n_steps):
     n_batches = int(len(arr) / batch_size)
     arr = arr[:batch_size * n_batches]
 
-    # 重塑
+    # 重塑  按照n_seqs进行划分为矩阵
     arr = arr.reshape((n_seqs,-1))
+    # arr.shape[1] 代表的是行数
     for n in range(0,arr.shape[1],n_steps):
         # inputs
         x = arr[:, n:n+n_steps]
@@ -37,19 +49,25 @@ def get_batches(arr,n_seqs,n_steps):
         y = np.zeros_like(x)
         y[:,:-1], y[:,-1] = x[:,1:], x[:,0]
         yield x, y
+# 上边的代码定义了一个generator，调用函数会返回一个generator对象，可以获取一个batch
+# bath大小为50  batch个数是10
 batches = get_batches(encoded,10,50)
+
 x, y = next(batches)
 
-print('x\n', x[:10, :10])
-print('\ny\n', y[:10, :10])  
+
+print('x\n', x[:, :])
+print('\ny\n', y[:, :])  
 
 # 构建输入层
 def build_inputs(num_seqs,num_steps):
     '''
-    num_seqs:每个batch中的序列个数
-    num_steps:每个序列包含的字符数
+    num_seqs:每个batch中的序列个数 64
+    num_steps:每个序列包含的字符数 50
     '''
+    # inputs 
     inputs = tf.placeholder(tf.int32,shape=(num_seqs,num_steps),name='inputs')
+    # targets 
     targets = tf.placeholder(tf.int32, shape=(num_seqs,num_steps),name='targets')
 
     # keep_prob dropout率
@@ -63,10 +81,11 @@ def build_lstm(lstm_size, num_layers,batch_size,keep_prob):
     num_layers:隐藏层的个数
     batch_size
     '''
-    lstm = tf.nn.rnn_cell.BasicLSTMCell(lstm_size)
-    drop = tf.nn.rnn_cell.DropoutWrapper(lstm,output_keep_prob=keep_prob)
+    lstm = tf.contrib.rnn.BasicLSTMCell(lstm_size)
+    drop = tf.contrib.rnn.DropoutWrapper(lstm,output_keep_prob=keep_prob)
     # 堆叠
-    cell = tf.nn.rnn_cell.MultiRNNCell([drop for _ in range(num_layers)])
+    # drop for _ in range(num_layers) num_layers个数目的drop列表
+    cell = tf.contrib.rnn.MultiRNNCell([drop for _ in range(num_layers)])
     initial_state = cell.zero_state(batch_size,tf.float32) 
     return cell, initial_state
 
@@ -77,14 +96,18 @@ def build_output(lstm_output, in_size,out_size):
     输出层重塑后的size
     softmax层的size
     '''
-    seq_output = tf.concat(1,lstm_output)
+    seq_output = tf.concat(lstm_output,axis=1)
     x = tf.reshape(seq_output,[-1,in_size])
 
     # 将lstm层和softmax层进行连接
     with tf.variable_scope('softmax'):
+        # stddev代表的是标准误差
         softmax_w = tf.Variable(tf.truncated_normal([in_size, out_size],stddev=0.1))
         softmax_b = tf.Variable(tf.zeros(out_size)) 
     # logits
+    print("#######################################")
+    print(x.dtype)
+    print(softmax_w)
     logits = tf.matmul(x,softmax_w) + softmax_b
 
     out = tf.nn.softmax(logits,name='predictions')
@@ -97,7 +120,7 @@ def build_loss(logits,targets,lstm_size,num_classes):
     num_classes vocab_size
     '''
     y_one_hot = tf.one_hot(targets,num_classes)
-    y_reshaped = tf.reshape(y_one_hot, logits.getshape())
+    y_reshaped = tf.reshape(y_one_hot, logits.get_shape())
 
     loss = tf.nn.softmax_cross_entropy_with_logits(logits=logits,labels=y_reshaped)
     loss = tf.reduce_mean(loss)
@@ -130,10 +153,14 @@ class CharRNN:
         # LSTM层
         cell, self.initial_state = build_lstm(lstm_size,num_layers,batch_size,self.keep_prob)
 
-        # 对输入进行one-hot编码
+        # 对输入进行one-hot编码 谁的是一维数据 后边代表的是dimension维度的意思
+        # num_classes 
         x_one_hot = tf.one_hot(self.inputs, num_classes)
 
+        print(x_one_hot)
         # 运行RNN
+        # outputs, state = tf.nn.dynamic_rnn(cell, x_one_hot, initial_state=self.initial_state)
+
         outputs, state = tf.nn.dynamic_rnn(cell, x_one_hot, initial_state=self.initial_state)
         self.final_state = state
 
@@ -161,8 +188,8 @@ keep_prob = 0.5
 epochs = 20
 save_every_n = 200
 model = CharRNN(len(vocab), batch_size=batch_size,num_steps=num_steps,
-                lstm_size=lstm_size,num_layers=num_layers,
-                learning_rate=learning_rate)
+                lstm_size=lstm_size,num_layers=num_layers,learning_rate=learning_rate,
+                grad_clip=5,sampling=True)
 
 saver = tf.train.Saver(max_to_keep=100)
 with tf.Session() as sess:
@@ -184,7 +211,7 @@ with tf.Session() as sess:
                                                 model.final_state,
                                                 model.optimizer],
                                                 feed_dict=feed)
-            end_time = time.time()
+            end = time.time()
             if counter % 100 == 0:
                 print('轮数： {} / {}...'.format(e+1, epochs),
                       '训练步数： {}...'.format(counter),
@@ -205,7 +232,6 @@ with tf.Session() as sess:
 # 预测结果概率最大的前五个为[o,e,i,u,b]，我们将随机从这五个中挑选一个作为新的字符，
 # 让过程加入随机因素会减少一些噪音的生成。
 
-# In[18]:
 
 def pick_top_n(preds, vocab_size, top_n=5):
     """
@@ -225,7 +251,6 @@ def pick_top_n(preds, vocab_size, top_n=5):
     return c
 
 
-# In[19]:
 
 def sample(checkpoint, n_samples, lstm_size, vocab_size, prime="The "):
     """
@@ -277,34 +302,20 @@ def sample(checkpoint, n_samples, lstm_size, vocab_size, prime="The "):
 
 # Here, pass in the path to a checkpoint and sample from the network.
 
-# In[20]:
-
 tf.train.latest_checkpoint('checkpoints')
-
-
-# In[26]:
 
 # 选用最终的训练参数作为输入进行文本生成
 checkpoint = tf.train.latest_checkpoint('checkpoints')
 samp = sample(checkpoint, 2000, lstm_size, len(vocab), prime="The")
 print(samp)
 
-
-# In[22]:
-
 checkpoint = 'checkpoints/i200_l512.ckpt'
 samp = sample(checkpoint, 1000, lstm_size, len(vocab), prime="Far")
 print(samp)
 
-
-# In[23]:
-
 checkpoint = 'checkpoints/i1000_l512.ckpt'
 samp = sample(checkpoint, 1000, lstm_size, len(vocab), prime="Far")
 print(samp)
-
-
-# In[24]:
 
 checkpoint = 'checkpoints/i2000_l512.ckpt'
 samp = sample(checkpoint, 1000, lstm_size, len(vocab), prime="Far")
