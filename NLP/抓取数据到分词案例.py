43453eb5a71009d5bc54df6dff31f043797ef139
liudong <ld815626@hotmail.com>
2018-08-03 09:41:46 +0800

    Update 抓取数据到分词案例.py

--- NLP/抓取数据到分词案例.py
+++ NLP/抓取数据到分词案例.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.rcParams['figure.figsize'] = (10.0,5.0)
-# from wordcloud import WordCloud 
+from wordcloud import WordCloud 
 
 
 
@@ -31,7 +31,7 @@ for item in nowplaying_movie_list:
 
 print(nowplaying_list)
 
-# 对电影的短评进行处理
+# 对电影的短评进行处理 抽取短评的数据
 requrl = "https://movie.douban.com/subject/" + nowplaying_list[0]['id'] + '/comments'+ '?' + 'staus=p' 
 resp = request.urlopen(requrl)
 html_data = resp.read().decode('utf-8')
@@ -61,14 +61,26 @@ words_df = pd.DataFrame({'segment':segment})
 print(words_df.head())
 
 # 去除停用词
-stopwords = pd.read_csv('D:/Users/sangfor/Desktop/chineseStopWords.txt',sep="\t",names=['stopword'], encoding='gb2312')
+stopwords = pd.read_csv('D:/Users/*****/Desktop/chineseStopWords.txt',sep="\t",names=['stopword'], encoding='gb2312')
 words_df = words_df[~words_df.segment.isin(stopwords.stopword)]
 print(words_df.head())
 
 # 词频统计
 words_stat =  words_df.groupby(by=['segment'])['segment'].agg({"计数":np.size})
 words_stat = words_stat.reset_index().sort_values(by=['计数'],ascending=False)
+print(words_stat)
 
 # 码云统计 
 # 其中simhei.ttf使用来指定字体的，
 # 可以在百度上输入simhei.ttf进行下载后，放入程序的根目录即可。
+wordcloud=WordCloud(font_path="D:/Users/****/Desktop/simhei.ttf",background_color="white",max_font_size=80) #指定字体类型、字体大小和字体颜色
+word_frequence = {x[0]:x[1] for x in words_stat.head(1000).values}
+word_frequence_list = []
+for key in word_frequence:
+    temp = (key,word_frequence[key])
+    word_frequence_list.append(temp)
+
+print(word_frequence_list) 
+wordcloud = wordcloud.fit_words(dict(word_frequence_list))
+plt.imshow(wordcloud)
+plt.savefig('D:/Users/****/Desktop/result.png')
